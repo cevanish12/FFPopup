d1282b59b7b974fbe40c168149336c65099d01d6
Casey Evanish <cevanish12@gmail.com>
2022-05-18 00:08:10 -0400

    fixing Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .target(
             name: "FFPopup",
-            path: "Classes",
+            path: "FFPopup/Classes",
             exclude: [
                 "FFPopup.m"
             ]
